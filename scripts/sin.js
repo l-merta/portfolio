21cd31de91fe26956460e80f79470f5cfa69d366
l-merta <164738919+l-merta@users.noreply.github.com>
2024-07-02 20:51:40 +0000

    push

--- scripts/sin.js
+++ scripts/sin.js
@@ -19,18 +19,8 @@ animate_sin();
 function animate_sin()
 {
   requestAnimationFrame(animate_sin);
-  sin_ctx.fillStyle = 'rgba(255,255,255,0.8)';
+  sin_ctx.fillStyle = 'rgba(255,255,255,0.5)';
   sin_ctx.fillRect(0, 0, sin_canvas.width, sin_canvas.height);
-
-  // Save the current state of the canvas
-  sin_ctx.save();
-
-  // Move the canvas origin to the center
-  sin_ctx.translate(sin_canvas.width / 2, sin_canvas.height / 2);
-  // Rotate the canvas context
-  sin_ctx.rotate(wave.tilt);
-  // Move the origin back to the top-left
-  sin_ctx.translate(-sin_canvas.width / 2, -sin_canvas.height / 2);
 
   sin_ctx.beginPath();
   sin_ctx.moveTo(0, 0); // Start at the top-left corner
@@ -49,10 +39,6 @@ function animate_sin()
   sin_ctx.strokeStyle = "black";
   sin_ctx.lineWidth = wave.lineWidth;
   sin_ctx.stroke();
-
-  // Restore the canvas to its original state
-  sin_ctx.restore();
-
   increment += wave.frequency;
 }
 
@@ -60,6 +46,6 @@ window.addEventListener('resize', resize_sin);
 
 function resize_sin()
 {
-  sin_canvas.width = sin_canvas_parent.clientWidth * 3.5;
-  sin_canvas.height = sin_canvas_parent.clientHeight * 3.5;
+  sin_canvas.width = sin_canvas_parent.clientWidth * 4.0;
+  sin_canvas.height = sin_canvas_parent.clientHeight * 5.0;
 }
