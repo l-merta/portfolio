fbbd1b67b827e83f473b50903d0fc98f93fe1f1f
l-merta <164738919+l-merta@users.noreply.github.com>
2024-08-03 19:40:08 +0000

    filmino add

--- scripts/projekty.js
+++ scripts/projekty.js
@@ -6,15 +6,18 @@ fetch('scripts/json/data.json')
 
 function renderWebsProjekty(webData) {
   console.log(webData);
-  webData.forEach(data => {
+  webData.forEach((data, index) => {
     const newWeb = webParentProjekty.querySelector(".webDef").cloneNode(true);
+    console.log(newWeb);
     newWeb.classList = "web";
     //
     newWeb.querySelector("img").src = "images/projekty/"+data.image;
     newWeb.querySelector("h1").innerHTML = data.name;
     newWeb.querySelector("p").innerHTML = data.popis;
-    if(data.link.length > 0)
+    if(data.link.length > 0) {
+      newWeb.querySelector(".link").href = data.link;
       newWeb.querySelector(".linkCont a").href = data.link;
+    }
     else
       newWeb.querySelector(".linkCont").classList.add("linkCont-inactive")
 
@@ -28,7 +31,8 @@ function renderWebsProjekty(webData) {
       techParent.appendChild(newTech, techParent.children[techParent.children.length]);
     });
     //
-    webParentProjekty.appendChild(newWeb, webParentProjekty.children[webParentProjekty.children.length]);
+    const webContPar = index % 2 != 0 || (webData.length % 2 == 0 ? index == -1 : index == 0) ? webParentProjekty.querySelector(".web-cont-1") : webParentProjekty.querySelector(".web-cont-2");
+    webContPar.appendChild(newWeb, webContPar.children[webContPar.children.length]);
   });
   //resize_sin();
 }
